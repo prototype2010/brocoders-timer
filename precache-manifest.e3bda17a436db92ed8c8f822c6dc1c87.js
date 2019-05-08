bf7bf3ae410f84860ff8b52b12646eb30b287328
Alexander Z <o.zakorko@soft-industry.com>
2019-05-08 20:43:08 +0300

    Updates

---
+++ precache-manifest.e3bda17a436db92ed8c8f822c6dc1c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74da60b3bdf26106bcac09858791976",
+    "url": "/brocoders-timer/index.html"
+  },
+  {
+    "revision": "e1cbd269409abd2af8d2",
+    "url": "/brocoders-timer/static/css/main.7446044b.chunk.css"
+  },
+  {
+    "revision": "4440c5d72c0709299ca9",
+    "url": "/brocoders-timer/static/js/2.98ed693e.chunk.js"
+  },
+  {
+    "revision": "e1cbd269409abd2af8d2",
+    "url": "/brocoders-timer/static/js/main.96082c91.chunk.js"
+  },
+  {
+    "revision": "d7e17bba7c4671b9c1b8",
+    "url": "/brocoders-timer/static/js/runtime~main.2182f1e0.js"
+  },
+  {
+    "revision": "45daaab7f76ba6ddea0fdefd85871316",
+    "url": "/brocoders-timer/static/media/404Error.45daaab7.jpg"
+  }
+]);
